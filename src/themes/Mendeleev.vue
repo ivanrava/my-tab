62b0f264e1493d0c75d276f15a24482f229fec0d
Ivan Ravasi <ivan4.ravasi@gmail.com>
2020-11-27 14:01:20 +0100

    Changes columns spacing and layout

--- src/themes/Mendeleev.vue
+++ src/themes/Mendeleev.vue
@@ -1,10 +1,12 @@
 <!-- Periodic table of the elements (by Mendeleev or Walter White, it's up to you) -->
 <template>
   <main>
+    <div class="column empty"></div>
     <div class="column" v-for="hive in hives" :key="hive.name">
       <h4>{{ hive.name }}</h4>
       <ChemicalElement v-for="link in hive.links" :link="link" :key="link.href"></ChemicalElement>
     </div>
+    <div class="column empty"></div>
   </main>
 </template>
 
@@ -28,12 +30,13 @@ main {
   font-family: 'Verdana', sans-serif;
   display: flex;
   flex-direction: row;
-  justify-content: space-evenly;
+  justify-content: center;
   background-color: #202020;
 }
 .column {
   display: flex;
   flex-direction: column-reverse;
+  flex-grow: 1;
 }
 h4 {
   text-align: center;
